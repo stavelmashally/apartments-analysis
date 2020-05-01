3b0b773747772271eb663b45e020c955275fa853
Stav Elmashally <stavelmashally6@gmail.com>
2020-05-01 17:57:22 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,34 @@
+AGENTS = [
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
+]
+
+HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'en-GB,en;q=0.9,en-US;q=0.8,hi;q=0.7,la;q=0.6',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'Pragma': 'no-cache',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+}
+
+DATA_PATH = 'data'
+
+URL = 'https://www.yad2.co.il/realestate/rent/map?city=5000&neighborhood={code}'
+ITEM_URL = 'https://www.yad2.co.il/item/{item_code}'
+
+NEIGBORHOODS = {
+    'newNorth': [204, 1516, 1519],
+    'oldNorth': [1483, 1461],
+}
---
+++ scraper.py
@@ -0,0 +1,33 @@
+import requests
+import time
+import config
+import random
+import os
+from bs4 import BeautifulSoup
+
+
+def to_html_file(html, area, code):
+    filename = f'{area}{code}.html'
+    with open(os.path.join(config.DATA_PATH, filename), 'w') as file:
+        file.write(html)
+
+
+def extract_info():
+    for file in os.listdir(config.DATA_PATH):
+        print(file)
+
+
+def scrape_apartments():
+    for area, codes in config.NEIGBORHOODS.items():
+        for code in codes:
+            headers = config.HEADERS
+            headers['User-Agent'] = random.choice(config.AGENTS)
+            res = requests.get(config.URL.format(code=code), headers=headers)
+            print(res.status_code)
+            to_html_file(res.text, area, code)
+            time.sleep(3)
+
+
+if __name__ == "__main__":
+    scrape_apartments()
+    extract_info()
